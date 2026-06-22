8a413a6bf9f563c70d5fb0bda34ea6e986984bcd
hiroyuki0504 <0504yamayama@gmail.com>
2026-06-22 17:04:11 +0900

    refactor: extract remote cost models

--- Veqral/AppState.swift
+++ Veqral/AppState.swift
@@ -8,42 +8,6 @@ import UserNotifications
 import UIKit
 #endif
 
-struct RemoteProjectCostSummary: Codable, Identifiable, Equatable, Sendable {
-    var projectKey: String
-    var projectID: String?
-    var workingDirectory: String?
-    var displayName: String
-    var runCount: Int
-    var inputTokens: Int
-    var outputTokens: Int
-    var reasoningTokens: Int
-    var totalTokens: Int
-    var estimatedCostUSD: Double
-    var actualCostUSD: Double
-    var costUSD: Double
-    var budgetLimitUSD: Double?
-    var thresholdPercent: Double
-    var paused: Bool
-    var isNearLimit: Bool
-    var isOverLimit: Bool
-
-    var id: String { projectKey }
-}
-
-struct RemoteProjectBudgetListResponse: Codable, Sendable {
-    var summaries: [RemoteProjectCostSummary]
-}
-
-struct RemoteProjectBudgetUpdateRequest: Codable, Sendable {
-    var projectKey: String?
-    var projectID: String?
-    var workingDirectory: String?
-    var displayName: String?
-    var limitUSD: Double?
-    var thresholdPercent: Double?
-    var paused: Bool?
-}
-
 struct RemoteGitDiffEntry: Codable, Equatable, Sendable {
     var path: String
     var additions: Int
