790ce4683d290c4cc111118bf1acff36650fa617
hiroyuki0504 <0504yamayama@gmail.com>
2026-06-22 17:04:28 +0900

    refactor: extract remote artifact models

--- Veqral/AppState.swift
+++ Veqral/AppState.swift
@@ -8,25 +8,6 @@ import UserNotifications
 import UIKit
 #endif
 
-struct RemoteArtifactRecord: Codable, Identifiable, Equatable, Sendable {
-    var id: String
-    var title: String
-    var type: String
-    var path: String
-    var bytes: Int
-    var updatedAt: Date?
-}
-
-struct RemoteArtifactListResponse: Codable, Sendable {
-    var artifacts: [RemoteArtifactRecord]
-}
-
-struct RemoteArtifactContentResponse: Codable, Sendable {
-    var artifactID: String
-    var mimeType: String
-    var data: Data
-}
-
 struct RemoteDeviceRecord: Codable, Identifiable, Equatable, Sendable {
     var id: String
     var name: String
