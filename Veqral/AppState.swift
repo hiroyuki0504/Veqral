16d360c3c617ae405e10f7ffcbcd4546adb78c44
hiroyuki0504 <0504yamayama@gmail.com>
2026-06-22 17:03:14 +0900

    refactor: extract remote stream models

--- Veqral/AppState.swift
+++ Veqral/AppState.swift
@@ -8,42 +8,6 @@ import UserNotifications
 import UIKit
 #endif
 
-struct RemoteHostLogEvent: Codable, Sendable {
-    var runID: String
-    var kind: String
-    var stream: String
-    var message: String
-    var createdAt: Date
-    var sessionID: String?
-    var exitCode: Int32?
-}
-
-enum RemoteStreamPhase: String, Equatable {
-    case idle
-    case connecting
-    case connected
-    case reconnecting
-    case disconnected
-}
-
-struct RemoteStreamStatus: Equatable {
-    var phase: RemoteStreamPhase
-    var runID: UUID?
-    var runTitle: String
-    var detail: String
-    var attempt: Int
-    var nextRetrySeconds: Int?
-
-    static let idle = RemoteStreamStatus(
-        phase: .idle,
-        runID: nil,
-        runTitle: "",
-        detail: "",
-        attempt: 0,
-        nextRetrySeconds: nil
-    )
-}
-
 struct RemoteCreateRunResponse: Codable, Sendable {
     var runID: String
     var sessionID: String?
